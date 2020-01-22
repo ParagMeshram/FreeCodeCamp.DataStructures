6ba7a21c2124081be5d4267d231e8fba8ff0157c
Parag Meshram <Parag_Meshram@symantec.com>
2020-01-22 16:49:40 +0530

    Added dynamic array example code

--- DynamicArray/Program.cs
+++ DynamicArray/Program.cs
@@ -8,6 +8,24 @@
         private static void Main()
         {
             var numbers = new Array<int>();
+            
+            numbers.Add(1);
+            numbers.Add(2);
+            numbers.Add(3);
+            numbers.Add(4);
+            numbers.Add(5);
+            numbers.Add(6);
+            numbers.Add(7);
+            numbers.Add(8);
+            numbers.Add(9);
+            numbers.Remove(0);
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
         }
     }
 }
